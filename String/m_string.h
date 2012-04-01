3468c14429f3bdc7be82873168b1250d192bf450
Larkina Olga <larkina.bad@gmail.com>
2012-04-01 19:09:21 +1100

    Add Copy_on_write to header: buffer, ref counting, proxy_char.

--- String/m_string.h
+++ String/m_string.h
@@ -4,13 +4,51 @@
 #include <istream>
 #include <ostream>
 #include <iostream>
+#include <algorithm>
 
 class String {
-	char* buff;
+	class Buffer{
+	public:
+		char* str;
+		int link_count;
+		int len;
+		
+		Buffer(): link_count(0), len(0){};
+		void check_link(){
+			if (link_count == 0)
+				free(str);
+		}
+	};
+
+	class proxy_char{
+		String& parent;
+		int idx;
+	public:
+		proxy_char(int i, String& s): idx(i), parent(s){};
+		proxy_char& operator=(char* t){
+			if(--parent.buff->link_count){
+				char* new_value = (char*)malloc(sizeof(char) * (parent.length() + 1));
+				strcpy(new_value, parent.buff->str);
+				parent.buff = new Buffer();
+				parent.buff->str = new_value;
+				parent.buff->len = strlen(new_value);
+			}
+			parent.buff->link_count = 1;
+			int l = strlen(t) > parent.length() + 1? parent.length() + 1 : strlen(t);
+			parent = parent.substr(0, idx) + t + parent.substr(idx + l, parent.length() - l + 1);
+			return *this;
+		}
+		operator char(){
+			return parent.buff->str[idx];
+		}
+	};
+	
+	friend class proxy_char;
+	Buffer* buff;
 
 public:
-	String();
-	String(const char* s);
+
+	String(const char* s = "");
 	String(const String& s);
 	~String();
 	
@@ -19,6 +57,10 @@ public:
 
 	String& operator=(const String& s);
 
+	int get_link_count()const {
+		return buff->link_count;
+	}
+
 	bool operator==( const String& s);
 	bool operator!=( const String& s);
 	bool operator<(const String& s);
@@ -26,14 +68,13 @@ public:
 	bool operator>(const String& s);
 	bool operator>=(const String& s);
 
-	char operator[](int idx);
-	String& substr(int idx, int len);
-	
-	friend String& operator+(String& a, String& b);
-	friend String& operator+(char* a, String& b);
-	friend String& operator+(String& a, char* b);
-	friend String& operator+=(String& a, String& b);
-	friend String& operator+=(String& a, char* b);
+	proxy_char operator[](int idx);
+	String substr(int idx, int len) const;
+	void insert(char* s, int idx);
+	void delete_substr(int idx, int len);
+
+	friend String operator+(const String& a, const String& b);
+	friend String& operator+=(String& a, const String& b);
 
 	friend std::ostream& operator<<(std::ostream&, const String&);
 	friend std::istream& operator>>(std::istream&, String&);
