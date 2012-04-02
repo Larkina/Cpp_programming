887ec3105e3fbfa02086c6b184b05dd5162b7d48
Larkina Olga <larkina.bad@gmail.com>
2012-04-02 22:18:56 +1100

    Add test for string

--- List/main.cpp
+++ List/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "list.h"
+#include <string.h>
 
 using namespace std;
 
@@ -27,7 +28,39 @@ void m_test(T get, T res, int line){
 }
 
 int main(){
-	List<int> l;
-	test(l.size(), 0);
+	{
+		List<int> l;
+		test(l.size(), 0);
+		test(l.empty(), true);
+		l.push_back(20);
+		List<int>::iterator it = l.begin();
+		l.insert(it, 666, 2);
+		test(*it, 20);
+		l.push_front(30);
+		it = l.begin();
+		test(*it, 30);
+		l.push_back(7);
+		it = --l.end();
+		test(*it, 7);
+		test(l.size(), 5);
+		it--;
+		test(*it, 20);
+		it = l.erase(it);
+		test(*it, 7);
+		l.erase(l.begin(), l.end());
+		test(l.empty(), true);
+	}
+	{
+		List<string> l;
+		string t = "abra";
+		l.push_front(t);
+		l.push_front("cadabra");
+		test(l.size(), 2);
+		List<string>::iterator it = l.end();
+		it--;
+		test((*it).compare(t), 0);
+		it = l.begin();
+		test((*(it++)).compare("cadabra"), 0);
+	}
 	print_result();
 }
